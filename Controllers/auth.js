ae4219835bff70aefd35effb3e37b718b8f0f459
danish45007 <myemail>
2020-06-11 19:10:46 +0530

    data validation added

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,13 +1,20 @@
 const User = require("../Models/user")
-
+const {check,validationResult} = require('express-validator')
 
 exports.signup = (req,res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0],
+            param: errors.array()[2]
+        })
+    }
     const user = new User(req.body)
     user.save((err,user) => {
         if(err){
             return res.status(400).json({
                 err: "Unable to save user to DB"
-            })
+            });
 
         } else{
             res.json(user)
--- Routers/auth.js
+++ Routers/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router()
 const {signout,signup} = require("../Controllers/auth");
+const {check, validationResult} = require('express-validator');
 
-
-router.post("/signup",signup)
+router.post("/signup",[
+    check("name")
+    .isLength({ min: 3 }).withMessage('must be at least 3 chars long'),
+    check("email").isEmail().withMessage("Email is required"),check("password").isLength({ min:5 }).withMessage("must be at least 3 chars long")
+], signup)
 
 
 router.get("/signout",signout)
